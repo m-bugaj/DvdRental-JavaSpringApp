16efa614cdfb0483b518201226d64e3cf590697b
m-bugaj <bbugi2016@gmail.com>
2024-07-22 16:07:48 +0200

    Added logging + saving logs to file

--- src/main/java/com/example/dvdRental/schedulded/ScheduldedTasks.java
+++ src/main/java/com/example/dvdRental/schedulded/ScheduldedTasks.java
@@ -4,17 +4,21 @@ import com.example.dvdRental.model.Customer;
 import com.example.dvdRental.model.Rental;
 import com.example.dvdRental.repositories.CustomerRepository;
 import com.example.dvdRental.repositories.RentalRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.logging.FileHandler;
 
+@Slf4j
 @Component
 public class ScheduldedTasks {
     RentalRepository rentalRepository;
@@ -47,7 +51,9 @@ public class ScheduldedTasks {
         customerRepository.saveAll(customers);
     }
 
-    @Scheduled(cron = "*/5 * * * * *")
+//    @Scheduled(cron = "*/10 * * * * *")
+    @Scheduled(cron = "0 0 7 * * *")
+//    @Scheduled(cron = "0 */2 * * * *")
     public void generateReportSchedulded() {
         LocalDateTime oneWeekAgo = LocalDate.now().minusWeeks(1).atTime(0,0);
         Timestamp dateOneWeekAgo = Timestamp.valueOf(oneWeekAgo);
@@ -57,7 +63,6 @@ public class ScheduldedTasks {
 
        for (Rental rental : rentals) {
            Integer customerId = rental.getCustomer().getCustomerId();
-//           System.out.println(customerId);
            List<String> films = reportHashMap.getOrDefault(customerId, new ArrayList<>());
            films.add(rental.getInventory().getFilm().getTitle());
            reportHashMap.put(customerId,films);
@@ -68,14 +73,33 @@ public class ScheduldedTasks {
 
            if (customerOptional.isPresent()) {
                Customer customer = customerOptional.get();
+
+               StringBuilder report = new StringBuilder();
+               report.append("\nID: ").append(customerId).append("\n");
+               report.append(customer.getFirstName()).append(" ").append(customer.getLastName()).append("\n");
+               report.append(customer.getEmail()).append("\n");
+               report.append("Wypożyczone filmy:\n");
+
                for (String filmTitle : reportHashMap.get(customerId)) {
-                   System.out.printf("ID: %d%n %s %s%n %s%n Film title: %s%n%n",
-                           customerId,
-                           customer.getFirstName(),
-                           customer.getLastName(),
-                           customer.getEmail(),
-                           filmTitle);
+                   report.append("- ").append(filmTitle).append("\n");
+
+//                   System.out.printf("ID: %d%n %s %s%n %s%n Film title: %s%n%n",
+//                           customerId,
+//                           customer.getFirstName(),
+//                           customer.getLastName(),
+//                           customer.getEmail(),
+//                           filmTitle);
                }
+               log.info(report.toString()); // Zapisanie raportu do logów
+//               SimpleDateFormat format = new SimpleDateFormat("M-d_HHmmss");
+//               try {
+//                   fh = new FileHandler("/logs/MyLogFile_"
+//                           + format.format(Calendar.getInstance().getTime()) + ".log");
+//                   log.
+//               } catch (SecurityException | IOException e) {
+//                   e.printStackTrace();
+//               }
+
            }
        }
     }
